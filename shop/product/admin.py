62a71ff46e421b235cb3517359d857a16946d79e
JianDk <jian_dfh@hotmail.com>
2020-03-09 21:03:30 +0100

    update

--- shop/product/admin.py
+++ shop/product/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+class productAdmin(admin.ModelAdmin):
+    #search_fields is a standard method. The strings in the list must match the model. These fields becomes searchable
+    search_fields = ['title', 'description', 'price']
+    #list_display is a method, all parameters given as a string in the list can be displayed in the admin site
+    list_display = ['title', 'description', 'active', 'price']
+    #Make the table in admin list editable
+    list_editable = ['description', 'price','active']
+    #Allow to sort 
+    list_filter = ['price', 'title']
+
+    class Meta:
+        model = Product
+
+admin.site.register(Product, productAdmin)
--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length = 200, null = False, blank = False)
+    description = models.TextField(null = True, blank = True)
+    price = models.DecimalField(decimal_places=0, max_digits = 10)
+    active = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.title
